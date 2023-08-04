4c5652c018d9250e605cef93c77bc3716deb751a
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-04 15:56:38 -0400

    Add component Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,20 @@
+import Logo from '../static/logo.svg';
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='logo'>
+        <img src={Logo} alt='Taskmate Logo' />
+        <span>Taskmate</span>
+      </div>
+      <div className='themeSelector'>
+        <span className='light activeTheme'></span>
+        <span className='medium'></span>
+        <span className='dark'></span>
+        <span className='gradientOne'></span>
+        <span className='gradientTwo'></span>
+        <span className='gradientThree'></span>
+      </div>
+    </header>
+  );
 };
