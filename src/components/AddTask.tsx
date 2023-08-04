03bbb35314d863cdd225601fe9a6c21e4c8def1e
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-04 15:57:01 -0400

    Add component AddTask

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,3 +1,16 @@
 export const AddTask = () => {
-  return <div>AddTask</div>;
+  return (
+    <section className='addTask'>
+      <form>
+        <input
+          type='text'
+          name='task'
+          autoComplete='off'
+          placeholder='add task'
+          maxLength={25}
+        />
+        <button type='submit'>Add</button>
+      </form>
+    </section>
+  );
 };
