ec28f6e4aeefb3b6ffea4fd79903b71b99a8c79e
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-04 17:27:25 -0400

    Create models folder for demo data

---
+++ src/models/data.js
@@ -0,0 +1,5 @@
+export const data = [
+  { id: 10001, name: 'TASK A', time: '00:00 12/31/2030' },
+  { id: 10002, name: 'TASK B', time: '00:00 12/31/2030' },
+  { id: 10003, name: 'TASK C', time: '00:00 12/31/2030' },
+];
