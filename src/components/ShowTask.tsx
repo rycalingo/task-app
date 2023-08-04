f530e877fcc91bfb0569583893be76a9be084343
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-04 16:01:05 -0400

    Add component ShowTask

--- src/components/ShowTask.tsx
+++ src/components/ShowTask.tsx
@@ -1,3 +1,33 @@
 export const ShowTask = () => {
-  return <div>ShowTask</div>;
+  const task = [
+    { id: '10001', name: 'TASK A', time: '00:00 06/31/2030' },
+    { id: '10002', name: 'TASK B', time: '00:00 06/31/2030' },
+    { id: '10003', name: 'TASK A', time: '00:00 06/31/2030' },
+  ];
+
+  return (
+    <section className='showTask'>
+      <p className='head'>
+        <span>
+          <span className='title'>Todo</span>
+          <span className='count'>0</span>
+        </span>
+        <span className='clearAll'>Clear All</span>
+      </p>
+      <ul>
+        {task.map((task) => (
+          <li key={task.id && task.id}>
+            <p>
+              <span className='name'>{task.name ? task.name : 'New Task'}</span>
+              <span className='time'>
+                {task.time ? task.time : '00:00 05/31/2030'}
+              </span>
+            </p>
+            <i className='bi bi-pencil-square'></i>
+            <i className='bi bi-trash'></i>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
